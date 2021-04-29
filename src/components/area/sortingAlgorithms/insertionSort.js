13593f66436f97d0d6294dc90339b5f6039ce693
DheerajGadwala <drgad24@gmail.com>
2021-04-29 20:04:11 +0530

    insertion-sort updates

--- src/components/area/sortingAlgorithms/insertionSort.js
+++ src/components/area/sortingAlgorithms/insertionSort.js
@@ -3,18 +3,14 @@ export const insertionSort = (props)=>{
     const helper = (set, i, j, delay, pos, newElement, type)=>{
         if(type==="push"){
             return new Promise(resolve=>{setTimeout(()=>{
-                console.log('pushElements', i, j, newElement);
                 set[j] = set[j-1];
                 props.setSample([...set]);
-                console.log(set);
                 resolve();
             }, delay);});
         }else if(type==="insert"){
             return new Promise(resolve=>{setTimeout(()=>{
-                console.log('insertElements', i, j, pos, newElement);
                 set[pos] = newElement;
                 props.setSample([...set]);
-                console.log(set);
                 resolve();
             }, delay);});
         }
@@ -23,25 +19,36 @@ export const insertionSort = (props)=>{
     async function main(){
         var set = [...props.sample]
         var i, j;
-        var delay=0;
-        const delayChange = 1000/props.sampleSize;
+        var delay=1000/props.sampleSize;
         props.setSorting(true);
-        console.log(props.sample);
+        document.getElementById((0)).classList.add('sortedBar');
         for(i=1; i<props.sampleSize; i++){
             var newElement=set[i];
             var pos=i;
+            document.getElementById((i)).classList.add('selectedBar');
             for(j=i; j>0; j--){
+                document.getElementById((j-1)).classList.add('blueBar');
                 if(set[j-1]>newElement){
-                    await helper(set, i, j, delayChange, pos, newElement, "push");
+                    await helper(set, i, j, delay, pos, newElement, "push");
                     pos-=1;
-                    delay+=delayChange;
+                    document.getElementById((j-1)).classList.remove('blueBar');
+                    document.getElementById((j-1)).classList.remove('sortedBar');
                 }else{
+                    document.getElementById((j-1)).classList.remove('blueBar');
                     break;
                 }
             }
-            await helper(set, i, j, delayChange, pos, newElement, "insert");
-            delay+=delayChange;
+            await helper(set, i, j, delay, pos, newElement, "insert");
+            document.getElementById((i)).classList.remove('selectedBar');
+            document.querySelectorAll('.bar').forEach(element=>{
+                if(element.id<=i){
+                    document.getElementById((element.id)).classList.add('sortedBar');
+                }
+            });
         }
     }
+props.setSorting(true);
 main();
+props.setSorting(false);
+props.setSorted(true);
 }
